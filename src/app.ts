0878a29c3e4a0a52e6ffa1d4e483f6d0d148a47b
thismajid <mkhorshidian72@gmail.com>
2022-05-21 11:44:58 +0430

    add custom logger to project

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,16 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 
+import morganMiddleware from "./middlewares/morgan.middleware";
+import logger from "./utils/logger";
+
 dotenv.config();
 
 const app: Express = express();
 const port: Number = Number(process.env.PORT) || 3000;
 
+app.use(morganMiddleware);
+
 app.listen(port, () => {
-  console.log(`Server is running on localhost:${port} ...`);
+  logger.info(`Server is running on localhost:${port} ...`);
 });
