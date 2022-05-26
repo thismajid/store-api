a880d803a829fdb4aeda41299cefab54c9e6eb69
thismajid <mkhorshidian72@gmail.com>
2022-05-26 19:18:40 +0430

    refactor issues

---
+++ src/prisma/seeds/categories.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.categories = void 0;
+exports.categories = [
+    {
+        id: 1,
+        title: 'men\'s clothing',
+    },
+    {
+        id: 2,
+        title: 'jewelery'
+    },
+    {
+        id: 3,
+        title: 'electronics'
+    }
+];
