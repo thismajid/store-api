5195881828253aea9266bf09d53dd48ec5b82826
thismajid <mkhorshidian72@gmail.com>
2022-05-26 18:09:21 +0430

    serve static files

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Express } from "express";
 import dotenv from "dotenv";
+import path from "path";
 
 import morganMiddleware from "./middlewares/morgan.middleware";
 import logger from "./utils/logger";
@@ -9,6 +10,8 @@ dotenv.config();
 
 const app: Express = express();
 const port: Number = Number(process.env.PORT) || 3000;
+
+app.use(express.static(path.join(__dirname, '..', 'public')))
 
 app.use(morganMiddleware);
 app.use(routes);
