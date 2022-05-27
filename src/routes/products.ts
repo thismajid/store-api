97a713b3884d18fae8d43cca94726815a1d31bc7
thismajid <mkhorshidian72@gmail.com>
2022-05-27 11:21:52 +0430

    add redis product

--- src/routes/products.ts
+++ src/routes/products.ts
@@ -1,16 +1,32 @@
 import { Router , Request, Response } from 'express';
 import { PrismaClient } from "@prisma/client";
+import Redis from 'ioredis';
 
 
 import {Routes} from './../interfaces/route.interface'
 import logger from "../utils/logger";
+import Redisio from "../services/redis.service";
 
 const prisma = new PrismaClient();
+// const RedisDb = new Redis({
+//     host: process.env.REDIS_HOST || '127.0.0.1',
+//     port: Number(process.env.REDIS_PORT || 6379),
+//     db: 6,
+//     keyPrefix: process.env.REDIS_PREFIX,
+// });
+
 
 
 export class ProductsRoute implements Routes{
     public router = Router();
     public path = '/products';
+    private productRedis = new Redis({
+        host: process.env.REDIS_HOST || '127.0.0.1',
+        port: Number(process.env.REDIS_PORT || 6379),
+        db: 6,
+        keyPrefix: process.env.REDIS_PREFIX,
+    });
+
 
     constructor() {
         this.initializeRoutes();
@@ -19,7 +35,14 @@ export class ProductsRoute implements Routes{
     private initializeRoutes() {
         this.router.get(this.path,async (req: Request,res: Response)=>{
             try{
-                const products = await prisma.product.findMany({});
+
+                const products = await prisma.product.findUnique({where: {id: 1}});
+
+                console.log(this.productRedis)
+                // this.productRedis.set(products)
+
+                // @ts-ignore
+                this.productRedis.set('products', JSON.stringify(products))
 
                 res.json({
                     message: 'all products retrieved successfully',
--- src/services/redis.service.ts
+++ src/services/redis.service.ts
@@ -15,17 +15,17 @@ class Redisio {
         });
     }
 
-    model(name = ""){
-        if(!instance.name){
-            this.connection();
-        }
-
-        return name ? {
-            hmset: (entity: any) => this.hmset(entity,name),
-        } : {
-            getInstance: () => instance[name],
-        }
-    }
+    // model(name = ""){
+    //     if(!instance.name){
+    //         this.connection();
+    //     }
+    //
+    //     return name ? {
+    //         hmset: (entity: any) => this.hmset(entity,name),
+    //     } : {
+    //         getInstance: () => instance[name],
+    //     }
+    // }
 
     getInstance(name: string) {
         if (instance === null) {
@@ -34,48 +34,50 @@ class Redisio {
         return instance[name];
     }
 
-    hmset(inputEntity: any, model: string) {
-        if (
-            ['products'].includes(model)
-        ) {
-            let entity = inputEntity;
-            let key = ``;
-            let strValue = '';
-            // let count = 0;
-
-            key += `${model}:${entity.id}`;
-            strValue = JSON.stringify(entity);
-            // for (let x of keys[model]) {
-            //     if (x.includes('.')) {
-            //         if (entity[x.split('.')[0]][x.split('.')[1]]) {
-            //             if (count != 0) {
-            //                 this.getInstance(model).zadd(
-            //                     `${model}.${x}.index`,
-            //                     0,
-            //                     `${entity[x.split('.')[0]][x.split('.')[1]]}:${entity.id}`
-            //                 );
-            //             }
-            //         }
-            //     } else if (entity[x]) {
-            //         if (count != 0) {
-            //             this.getInstance(model).zadd(
-            //                 `${model}.${x}.index`,
-            //                 0,
-            //                 `${entity[x]}:${entity.id}`
-            //             );
-            //         }
-            //     }
-            //     count++;
-            // }
-
-            this.getInstance(model).hmset(key, { value: strValue });
-            this.getInstance(model).expire(key, usersTtl);
-
-            // entity = ``;
-            // key = ``;
-            // strValue = '';
-        }
-    }
+    // hmset(inputEntity: any, model: string) {
+    //     if (
+    //         ['products'].includes(model)
+    //     ) {
+    //         let entity = inputEntity;
+    //         let key = ``;
+    //         let strValue = '';
+    //         console.log('entity', entity)
+    //         // let count = 0;
+    //
+    //         key += `${model}:${entity.id}`;
+    //         strValue = JSON.stringify(entity);
+    //         console.log("strValue", strValue)
+    //         // for (let x of keys[model]) {
+    //         //     if (x.includes('.')) {
+    //         //         if (entity[x.split('.')[0]][x.split('.')[1]]) {
+    //         //             if (count != 0) {
+    //         //                 this.getInstance(model).zadd(
+    //         //                     `${model}.${x}.index`,
+    //         //                     0,
+    //         //                     `${entity[x.split('.')[0]][x.split('.')[1]]}:${entity.id}`
+    //         //                 );
+    //         //             }
+    //         //         }
+    //         //     } else if (entity[x]) {
+    //         //         if (count != 0) {
+    //         //             this.getInstance(model).zadd(
+    //         //                 `${model}.${x}.index`,
+    //         //                 0,
+    //         //                 `${entity[x]}:${entity.id}`
+    //         //             );
+    //         //         }
+    //         //     }
+    //         //     count++;
+    //         // }
+    //
+    //         this.getInstance(model).hmset(key, { value: strValue });
+    //         this.getInstance(model).expire(key, usersTtl);
+    //
+    //         // entity = ``;
+    //         // key = ``;
+    //         // strValue = '';
+    //     }
+    // }
 }
 
 export default Redisio
