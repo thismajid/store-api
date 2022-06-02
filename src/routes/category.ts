2aadf89af39c1d73ea2b361f29d17a9d807e200c
thismajid <mkhorshidian72@gmail.com>
2022-06-02 18:56:03 +0430

    add category route

---
+++ src/routes/category.ts
@@ -0,0 +1,18 @@
+import { Router } from "express";
+
+import { Routes } from "./../interfaces/route.interface";
+import CategoryController from "../controllers/category.controller";
+
+export class CategoryRoute implements Routes {
+  public router = Router();
+  public path = "/categories";
+  private controller = new CategoryController();
+
+  constructor() {
+    this.initializeRoutes();
+  }
+
+  private initializeRoutes() {
+    this.router.get(this.path, this.controller.getAllCategories);
+  }
+}
