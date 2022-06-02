6319368de5b5792941ee0ae4dcbb534e8ae37622
thismajid <mkhorshidian72@gmail.com>
2022-06-02 11:34:23 +0430

    add redis to cache products in a category

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,83 +1,102 @@
-import {Request, Response, NextFunction} from "express";
+import { Request, Response, NextFunction } from "express";
 import { PrismaClient } from "@prisma/client";
 
 import logger from "../utils/logger";
 import Redisio from "../services/redis.service";
 
 const prisma = new PrismaClient();
-const productRedis = new Redisio('products')
+const productRedis = new Redisio("products");
+const productsCategoryRedis = new Redisio("productsInCategory");
 
 class ProductController {
-    constructor() {
-    }
+  constructor() {}
 
-    public async getAllProducts(req: Request, res: Response, next: NextFunction){
-        try{
-            let products = await productRedis.hget('products');
-            if(!products || products.length === 0) {
-                products = await prisma.product.findMany({});
-                productRedis.hmset(products, 'products' )
-            }
-            res.json({
-                message: 'all products retrieved successfully',
-                products
-            })
-        }catch(err){
-            logger.error(err)
-        }
+  public async getAllProducts(req: Request, res: Response, next: NextFunction) {
+    try {
+      let products = await productRedis.hget("products");
+      if (!products || products.length === 0) {
+        products = await prisma.product.findMany({});
+        productRedis.hmset(products, "products");
+      }
+      res.json({
+        message: "all products retrieved successfully",
+        products,
+      });
+    } catch (err) {
+      logger.error(err);
     }
+  }
 
-    public async getSingleProduct(req: Request, res: Response, next: NextFunction){
-        try{
-            const {id} = req.params
-            let product = await productRedis.hget('products', {id: +id})
-            if(!product){
-                product = await prisma.product.findUnique({where: {id: +id}})
-                productRedis.hmset(product, 'products' )
-            }
-            res.json({
-                message: 'product retrieved successfully',
-                product
-            })
-        }catch(err){
-            logger.error(err)
-        }
+  public async getSingleProduct(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) {
+    try {
+      const { id } = req.params;
+      let product = await productRedis.hget("products", { id: +id });
+      if (!product) {
+        product = await prisma.product.findUnique({ where: { id: +id } });
+        productRedis.hmset(product, "products");
+      }
+      res.json({
+        message: "product retrieved successfully",
+        product,
+      });
+    } catch (err) {
+      logger.error(err);
     }
+  }
 
-    public async getAllProductsInCategory(req: Request, res: Response, next: NextFunction){
-        try{
-            const {categoryName} = req.params;
-            const category = await prisma.category.findFirst(
-                {
-                    where: {title: categoryName}
-                });
-            if(!category){
-                // todo
-            }
-            const products = category && await prisma.product.findMany({
-                where: {
-                    categoryId: category.id
+  public async getAllProductsInCategory(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) {
+    try {
+      const { categoryName } = req.params;
+      let products;
+      products = await productsCategoryRedis.hget("productsInCategory", {
+        modelName: categoryName,
+      });
+      if (!products) {
+        const category = await prisma.category.findFirst({
+          where: {
+            title: categoryName,
+          },
+        });
+        products =
+          category &&
+          (await prisma.product.findMany({
+            where: {
+              categoryId: category.id,
+            },
+            include: {
+              category: true,
+              author: {
+                select: {
+                  id: true,
+                  firstname: true,
+                  lastname: true,
+                  email: true,
                 },
-                include: {
-                    category: true,
-                    author: {
-                        select: {
-                            id: true,
-                            firstname: true,
-                            lastname: true,
-                            email: true
-                        }
-                    }
-                }
-            });
-            res.json({
-                message: 'all products retrieved successfully',
-                products
-            })
-        }catch(err){
-            logger.error(err);
-        }
+              },
+            },
+          }));
+        await productsCategoryRedis.hmset(
+          products,
+          "productsInCategory",
+          categoryName
+        );
+      }
+      res.json({
+        message: "all products retrieved successfully",
+        products,
+      });
+    } catch (err) {
+      logger.error(err);
     }
+  }
 }
 
 export default ProductController;
