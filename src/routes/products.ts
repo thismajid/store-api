5aa1bf298dff7625e5ad52bbf3cd0570f0c58a68
thismajid <mkhorshidian72@gmail.com>
2022-06-02 18:56:18 +0430

    format code

--- src/routes/products.ts
+++ src/routes/products.ts
@@ -1,25 +1,25 @@
-import { Router } from 'express';
+import { Router } from "express";
 
-import {Routes} from './../interfaces/route.interface'
+import { Routes } from "./../interfaces/route.interface";
 import ProductController from "../controllers/product.controller";
 
-export class ProductsRoute implements Routes{
-    public router = Router();
-    public path = '/products';
-    private controller = new ProductController()
+export class ProductsRoute implements Routes {
+  public router = Router();
+  public path = "/products";
+  private controller = new ProductController();
 
-    constructor() {
-        this.initializeRoutes();
-    }
+  constructor() {
+    this.initializeRoutes();
+  }
 
-    private initializeRoutes() {
-        this.router.get(this.path, this.controller.getAllProducts);
+  private initializeRoutes() {
+    this.router.get(this.path, this.controller.getAllProducts);
 
-        this.router.get(`${this.path}/:id`, this.controller.getSingleProduct);
+    this.router.get(`${this.path}/:id`, this.controller.getSingleProduct);
 
-        this.router.get(`${this.path}/category/:categoryName`, this.controller.getAllProductsInCategory)
-    }
+    this.router.get(
+      `${this.path}/category/:categoryName`,
+      this.controller.getAllProductsInCategory
+    );
+  }
 }
-
-
-
