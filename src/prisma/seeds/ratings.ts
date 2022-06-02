0baca78824d341bdde77e4b29e29f637b795d4d7
thismajid <mkhorshidian72@gmail.com>
2022-06-02 17:29:23 +0430

    add ratings seed for products

---
+++ src/prisma/seeds/ratings.ts
@@ -0,0 +1,52 @@
+export const ratings = [
+    {
+        id: 1,
+        rate: 3.9,
+        count: 120,
+    },
+    {
+        id: 2,
+        rate: 4.1,
+        count: 259,
+    },
+    {
+        id: 3,
+        rate: 2.1,
+        count: 430,
+    },
+    {
+        id: 4,
+        rate: 2.1,
+        count: 430,
+    },
+    {
+        id: 5,
+        rate: 4.6,
+        count: 400,
+    },
+    {
+        id: 6,
+        rate: 3.9,
+        count: 70,
+    },
+    {
+        id: 7,
+        rate: 3,
+        count: 400,
+    },
+    {
+        id: 8,
+        rate: 1.9,
+        count: 100,
+    },
+    {
+        id: 9,
+        rate: 3.3,
+        count: 203,
+    },
+    {
+        id: 10,
+        rate: 2.9,
+        count: 470,
+    },
+];
