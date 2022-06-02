3aa81f67e13a00c64e37e75723e27ab19cf5152c
thismajid <mkhorshidian72@gmail.com>
2022-06-02 11:34:49 +0430

    some changes in hmset and hget

--- src/services/redis.service.ts
+++ src/services/redis.service.ts
@@ -1,116 +1,115 @@
-import Redis from 'ioredis';
+import Redis from "ioredis";
 import logger from "../utils/logger";
 
-import {options, usersTtl} from "./../configs/redis.config";
+import { options, usersTtl } from "./../configs/redis.config";
 import conditionType from "../interfaces/redis/condition.interface";
 
 let instance: any = null;
 
 class Redisio {
-    constructor(name: string) {
-        this.getInstance(name)
+  constructor(name: string) {
+    this.getInstance(name);
+  }
+
+  connection() {
+    instance = {
+      products: new Redis({ ...options, db: 7 }),
+      productsInCategory: new Redis({ ...options, db: 8 }),
+    };
+    Object.assign(instance, {
+      redisClient1: instance.products,
+      redisClient8: instance.productsInCategory,
+    });
+  }
+
+  // model(name = ""){
+  //     if(!instance.name){
+  //         this.connection();
+  //     }
+  //
+  //     return name ? {
+  //         hmset: (entity: any) => this.hmset(entity,name),
+  //     } : {
+  //         getInstance: () => instance[name],
+  //     }
+  // }
+
+  getInstance(name: string) {
+    if (instance === null) {
+      this.connection();
     }
+    return instance[name];
+  }
 
-    connection() {
-        instance = {
-            products: new Redis({...options, db: 7})
-        }
-        Object.assign(instance, {
-            redisClient1: instance.products,
-        });
+  hmset(inputs: any, model: string, keyName?: string) {
+    if (["products", "productsInCategory"].includes(model)) {
+      let key;
+      if (Array.isArray(inputs)) {
+        key = keyName ? `${model}:${keyName}` : `${model}`;
+      } else {
+        key = `${model}:${inputs.id}`;
+      }
+      this.getInstance(model).set(key, JSON.stringify(inputs));
     }
+  }
 
-    // model(name = ""){
-    //     if(!instance.name){
-    //         this.connection();
-    //     }
-    //
-    //     return name ? {
-    //         hmset: (entity: any) => this.hmset(entity,name),
-    //     } : {
-    //         getInstance: () => instance[name],
-    //     }
-    // }
-
-    getInstance(name: string) {
-        if (instance === null) {
-            this.connection();
-        }
-        return instance[name];
+  async hget(model: string, condition?: conditionType) {
+    try {
+      let key = `${model}`;
+      key = condition?.id
+        ? `${model}:${condition?.id}`
+        : `${model}:${condition?.modelName}`;
+      const data = await this.getInstance(model).get(key);
+      return JSON.parse(data);
+    } catch (err) {
+      logger.error(err);
     }
+  }
 
-    hmset(inputs: any, model: string){
-        if(['products'].includes(model)){
-            let key
-            if(Array.isArray(inputs)){
-                key = `${model}`;
-            } else {
-                key = `${model}:${inputs.id}`;
-            }
-            this.getInstance(model).set(key, JSON.stringify(inputs))
-
-        }
-    }
-
-    async hget(model: string, condition?: conditionType){
-        try{
-            let key = `${model}`;
-            // @ts-ignore
-            if(condition?.id) {
-                // @ts-ignore
-                key = `${model}:${condition?.id}`
-            }
-            const data = await this.getInstance(model).get(key);
-            return JSON.parse(data)
-        }catch(err){
-            logger.error(err)
-        }
-    }
-
-    // hmset(inputEntity: any, model: string) {
-    //     if (
-    //         ['products'].includes(model)
-    //     ) {
-    //         let entity = inputEntity;
-    //         let key = ``;
-    //         let strValue = '';
-    //         console.log('entity', entity)
-    //         // let count = 0;
-    //
-    //         key += `${model}:${entity.id}`;
-    //         strValue = JSON.stringify(entity);
-    //         console.log("strValue", strValue)
-    //         // for (let x of keys[model]) {
-    //         //     if (x.includes('.')) {
-    //         //         if (entity[x.split('.')[0]][x.split('.')[1]]) {
-    //         //             if (count != 0) {
-    //         //                 this.getInstance(model).zadd(
-    //         //                     `${model}.${x}.index`,
-    //         //                     0,
-    //         //                     `${entity[x.split('.')[0]][x.split('.')[1]]}:${entity.id}`
-    //         //                 );
-    //         //             }
-    //         //         }
-    //         //     } else if (entity[x]) {
-    //         //         if (count != 0) {
-    //         //             this.getInstance(model).zadd(
-    //         //                 `${model}.${x}.index`,
-    //         //                 0,
-    //         //                 `${entity[x]}:${entity.id}`
-    //         //             );
-    //         //         }
-    //         //     }
-    //         //     count++;
-    //         // }
-    //
-    //         this.getInstance(model).hmset(key, { value: strValue });
-    //         this.getInstance(model).expire(key, usersTtl);
-    //
-    //         // entity = ``;
-    //         // key = ``;
-    //         // strValue = '';
-    //     }
-    // }
+  // hmset(inputEntity: any, model: string) {
+  //     if (
+  //         ['products'].includes(model)
+  //     ) {
+  //         let entity = inputEntity;
+  //         let key = ``;
+  //         let strValue = '';
+  //         console.log('entity', entity)
+  //         // let count = 0;
+  //
+  //         key += `${model}:${entity.id}`;
+  //         strValue = JSON.stringify(entity);
+  //         console.log("strValue", strValue)
+  //         // for (let x of keys[model]) {
+  //         //     if (x.includes('.')) {
+  //         //         if (entity[x.split('.')[0]][x.split('.')[1]]) {
+  //         //             if (count != 0) {
+  //         //                 this.getInstance(model).zadd(
+  //         //                     `${model}.${x}.index`,
+  //         //                     0,
+  //         //                     `${entity[x.split('.')[0]][x.split('.')[1]]}:${entity.id}`
+  //         //                 );
+  //         //             }
+  //         //         }
+  //         //     } else if (entity[x]) {
+  //         //         if (count != 0) {
+  //         //             this.getInstance(model).zadd(
+  //         //                 `${model}.${x}.index`,
+  //         //                 0,
+  //         //                 `${entity[x]}:${entity.id}`
+  //         //             );
+  //         //         }
+  //         //     }
+  //         //     count++;
+  //         // }
+  //
+  //         this.getInstance(model).hmset(key, { value: strValue });
+  //         this.getInstance(model).expire(key, usersTtl);
+  //
+  //         // entity = ``;
+  //         // key = ``;
+  //         // strValue = '';
+  //     }
+  // }
 }
 
-export default Redisio
+export default Redisio;
