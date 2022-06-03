8f4774a27deef2706add209caf0adbf92f2a8d27
thismajid <mkhorshidian72@gmail.com>
2022-06-03 10:53:12 +0430

    update seeder function

--- src/prisma/seed.ts
+++ src/prisma/seed.ts
@@ -1,27 +1,31 @@
 import { PrismaClient } from "@prisma/client";
-import {createAdminUser} from "./seeds/create-admin-user";
-import {categories} from './seeds/categories'
-import {products} from "./seeds/products";
+import { createAdminUser } from "./seeds/create-admin-user";
+import { categories } from "./seeds/categories";
+import { products } from "./seeds/products";
+import { ratings } from "./seeds/ratings";
 
 const prisma = new PrismaClient();
 
 async function main() {
-    await prisma.user.createMany({
-        data: createAdminUser
-    })
-    await prisma.category.createMany({
-        data: categories
-    })
-    await prisma.product.createMany({
-        data: products
-    })
+  await prisma.user.createMany({
+    data: createAdminUser,
+  });
+  await prisma.category.createMany({
+    data: categories,
+  });
+  await prisma.rating.createMany({
+    data: ratings,
+  });
+  await prisma.product.createMany({
+    data: products,
+  });
 }
 
 main()
-    .catch((e) => {
-        console.error(e);
-        process.exit(1);
-    })
-    .finally(async () => {
-        await prisma.$disconnect();
-    });
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
