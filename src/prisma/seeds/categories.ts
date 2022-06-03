3191c82bdc0252a02d5c6a58cf94b73358977bde
thismajid <mkhorshidian72@gmail.com>
2022-06-03 10:52:14 +0430

    update categories data seed

--- src/prisma/seeds/categories.ts
+++ src/prisma/seeds/categories.ts
@@ -1,14 +1,18 @@
 export const categories = [
-    {
-        id: 1,
-        title: 'men\'s clothing',
-    },
-    {
-        id: 2,
-        title: 'jewelery'
-    },
-    {
-        id: 3,
-        title: 'electronics'
-    }
-]
+  {
+    id: 1,
+    title: "men's clothing",
+  },
+  {
+    id: 2,
+    title: "jewelery",
+  },
+  {
+    id: 3,
+    title: "electronics",
+  },
+  {
+    id: 4,
+    title: "women's clothing",
+  },
+];
