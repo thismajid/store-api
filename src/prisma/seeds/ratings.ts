1bd112bbffc13885ff9ba8904bcce79b7fb63790
thismajid <mkhorshidian72@gmail.com>
2022-06-03 10:52:47 +0430

    update ratings data seed

--- src/prisma/seeds/ratings.ts
+++ src/prisma/seeds/ratings.ts
@@ -1,52 +1,102 @@
 export const ratings = [
-    {
-        id: 1,
-        rate: 3.9,
-        count: 120,
-    },
-    {
-        id: 2,
-        rate: 4.1,
-        count: 259,
-    },
-    {
-        id: 3,
-        rate: 2.1,
-        count: 430,
-    },
-    {
-        id: 4,
-        rate: 2.1,
-        count: 430,
-    },
-    {
-        id: 5,
-        rate: 4.6,
-        count: 400,
-    },
-    {
-        id: 6,
-        rate: 3.9,
-        count: 70,
-    },
-    {
-        id: 7,
-        rate: 3,
-        count: 400,
-    },
-    {
-        id: 8,
-        rate: 1.9,
-        count: 100,
-    },
-    {
-        id: 9,
-        rate: 3.3,
-        count: 203,
-    },
-    {
-        id: 10,
-        rate: 2.9,
-        count: 470,
-    },
+  {
+    id: 1,
+    rate: 3.9,
+    count: 120,
+  },
+  {
+    id: 2,
+    rate: 4.1,
+    count: 259,
+  },
+  {
+    id: 3,
+    rate: 2.1,
+    count: 430,
+  },
+  {
+    id: 4,
+    rate: 2.1,
+    count: 430,
+  },
+  {
+    id: 5,
+    rate: 4.6,
+    count: 400,
+  },
+  {
+    id: 6,
+    rate: 3.9,
+    count: 70,
+  },
+  {
+    id: 7,
+    rate: 3,
+    count: 400,
+  },
+  {
+    id: 8,
+    rate: 1.9,
+    count: 100,
+  },
+  {
+    id: 9,
+    rate: 3.3,
+    count: 203,
+  },
+  {
+    id: 10,
+    rate: 2.9,
+    count: 470,
+  },
+  {
+    id: 11,
+    rate: 4.8,
+    count: 319,
+  },
+  {
+    id: 12,
+    rate: 4.8,
+    count: 400,
+  },
+  {
+    id: 13,
+    rate: 2.9,
+    count: 250,
+  },
+  {
+    id: 14,
+    rate: 2.2,
+    count: 140,
+  },
+  {
+    id: 15,
+    rate: 2.6,
+    count: 235,
+  },
+  {
+    id: 16,
+    rate: 2.9,
+    count: 340,
+  },
+  {
+    id: 17,
+    rate: 3.8,
+    count: 679,
+  },
+  {
+    id: 18,
+    rate: 4.7,
+    count: 130,
+  },
+  {
+    id: 19,
+    rate: 4.5,
+    count: 146,
+  },
+  {
+    id: 20,
+    rate: 3.6,
+    count: 145,
+  },
 ];
