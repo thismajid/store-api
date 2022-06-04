8684033728aebad54a24493bd59c3fda09854c65
thismajid <mkhorshidian72@gmail.com>
2022-06-04 17:25:27 +0430

    update get all products controllers

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -24,11 +24,7 @@ class ProductController {
                 lastname: true,
               },
             },
-            category: {
-              select: {
-                title: true,
-              },
-            },
+            categories: { include: { category: true } },
             rating: {
               select: {
                 count: true,
@@ -48,99 +44,178 @@ class ProductController {
     }
   }
 
-  public async getSingleProduct(
-    req: Request,
-    res: Response,
-    next: NextFunction
-  ) {
-    try {
-      const { id } = req.params;
-      let product = await productRedis.hget("products", { id: +id });
-      if (!product) {
-        product = await prisma.product.findUnique({
-          where: {
-            id: +id,
-          },
-          include: {
-            author: {
-              select: {
-                firstname: true,
-                lastname: true,
-              },
-            },
-            category: {
-              select: {
-                title: true,
-              },
-            },
-            rating: {
-              select: {
-                count: true,
-                rate: true,
-              },
-            },
-          },
-        });
-        productRedis.hmset(product, "products");
-      }
-      res.json({
-        message: "product retrieved successfully",
-        product,
-      });
-    } catch (err) {
-      logger.error(err);
-    }
-  }
+  //   public async addProduct(req: Request, res: Response, next: NextFunction) {
+  //     try {
+  //       const { title, description, price, category, image } = req.body;
+  //       const categoryId = await prisma.category.findFirst({
+  //         where: {
+  //           title: category,
+  //         },
+  //         select: {
+  //           id: true,
+  //         },
+  //       });
+  //       if (!categoryId?.id) {
+  //         //todo
+  //       }
+  //       // await prisma.rating.create({
+  //       //   data: {
+  //       //     id: 21,
+  //       //     rate: 0,
+  //       //     count: 0,
+  //       //   },
+  //       // });
+  //       const newProduct = await prisma.product.create({
+  //         data: {
+  //           title,
+  //           price,
+  //           description,
+  //           authorId: 1,
+  //           categoryId: 1,
+  //           image,
+  //           ratingId: 3,
+  //         },
+  //       });
 
-  public async getAllProductsInCategory(
-    req: Request,
-    res: Response,
-    next: NextFunction
-  ) {
-    try {
-      const { categoryName } = req.params;
-      let products;
-      products = await productsCategoryRedis.hget("productsInCategory", {
-        modelName: categoryName,
-      });
-      if (!products) {
-        const category = await prisma.category.findFirst({
-          where: {
-            title: categoryName,
-          },
-        });
-        products =
-          category &&
-          (await prisma.product.findMany({
-            where: {
-              categoryId: category.id,
-            },
-            include: {
-              category: true,
-              author: {
-                select: {
-                  id: true,
-                  firstname: true,
-                  lastname: true,
-                  email: true,
-                },
-              },
-            },
-          }));
-        await productsCategoryRedis.hmset(
-          products,
-          "productsInCategory",
-          categoryName
-        );
-      }
-      res.json({
-        message: "all products retrieved successfully",
-        products,
-      });
-    } catch (err) {
-      logger.error(err);
-    }
-  }
+  //       // const newProduct = await prisma.product.upsert({
+  //       //   where: { id: 21 },
+  //       //   update: {
+  //       //     title,
+  //       //     description,
+  //       //     price,
+  //       //     authorId: 1,
+  //       //     categoryId: categoryId?.id,
+  //       //     image,
+  //       //     rating: {
+  //       //       create: {
+  //       //         count: 0,
+  //       //         rate: 0,
+  //       //       },
+  //       //     },
+  //       //   },
+  //       //   create: {
+  //       //     title,
+  //       //     description,
+  //       //     price,
+  //       //     authorId: 1,
+  //       //     categoryId: categoryId?.id,
+  //       //     image,
+  //       //     rating: {
+  //       //       create: {
+  //       //         count: 0,
+  //       //         rate: 0,
+  //       //       },
+  //       //     },
+  //       //   },
+  //       // });
+  //       // const newRating = await prisma.rating.create({
+  //       //   data: {
+  //       //     id: 21,
+  //       //   },
+  //       // });
+  //       // const newProduct =
+  //       //   categoryId &&
+  //       //   (await prisma.product.create({
+  //       //     data: {
+  //       //       id: 21,
+
+  //       //     },
+  //       //   }));
+  //       res.json(newProduct);
+  //     } catch (err) {
+  //       logger.error(err);
+  //     }
+  //   }
+
+  //   public async getSingleProduct(
+  //     req: Request,
+  //     res: Response,
+  //     next: NextFunction
+  //   ) {
+  //     try {
+  //       const { id } = req.params;
+  //       let product = await productRedis.hget("products", { id: +id });
+  //       if (!product) {
+  //         product = await prisma.product.findUnique({
+  //           where: {
+  //             id: +id,
+  //           },
+  //           include: {
+  //             author: {
+  //               select: {
+  //                 firstname: true,
+  //                 lastname: true,
+  //               },
+  //             },
+  //             categories: { include: { category: true } },
+  //             rating: {
+  //               select: {
+  //                 count: true,
+  //                 rate: true,
+  //               },
+  //             },
+  //           },
+  //         });
+  //         productRedis.hmset(product, "products");
+  //       }
+  //       res.json({
+  //         message: "product retrieved successfully",
+  //         product,
+  //       });
+  //     } catch (err) {
+  //       logger.error(err);
+  //     }
+  //   }
+
+  //   public async getAllProductsInCategory(
+  //     req: Request,
+  //     res: Response,
+  //     next: NextFunction
+  //   ) {
+  //     try {
+  //       const { categoryName } = req.params;
+  //       let products;
+  //       products = await productsCategoryRedis.hget("productsInCategory", {
+  //         modelName: categoryName,
+  //       });
+  //       if (!products) {
+  //         const category = await prisma.category.findFirst({
+  //           where: {
+  //             title: categoryName,
+  //           },
+  //         });
+  //         products =
+  //           category &&
+  //           (await prisma.product.findMany({
+  //             where: {
+  //               categoryId: category.id,
+  //             },
+  //             include: {
+  //               category: true,
+  //               author: {
+  //                 select: {
+  //                   id: true,
+  //                   firstname: true,
+  //                   lastname: true,
+  //                   email: true,
+  //                 },
+  //               },
+  //             },
+  //           }));
+  //         await productsCategoryRedis.hmset(
+  //           products,
+  //           "productsInCategory",
+  //           categoryName
+  //         );
+  //       }
+  //       res.json({
+  //         message: "all products retrieved successfully",
+  //         products,
+  //       });
+  //     } catch (err) {
+  //       logger.error(err);
+  //     }
+  //   }
 }
 
 export default ProductController;
