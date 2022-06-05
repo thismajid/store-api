d0158e8ae686fcf34af73da190f50841449a64b6
thismajid <mkhorshidian72@gmail.com>
2022-06-05 17:06:25 +0430

    validator middleware

---
+++ src/middlewares/validator.middleware.ts
@@ -0,0 +1,22 @@
+// import createHttpError from "http-errors";
+import { NextFunction, Request, Response } from "express";
+import { ObjectSchema } from "joi";
+import UnprocessableEntityException from "../exceptions/UnprocessableEntityException";
+
+export function Validator(schema: ObjectSchema) {
+  return (req: Request, res: Response, next: NextFunction) => {
+    const params = req.method === "GET" ? req.params : req.body;
+    const { error } = schema.validate(params, {
+      abortEarly: false,
+      messages: {
+        key: "{{key}} ",
+      },
+    });
+    if (error) {
+      const { message } = error;
+      throw new UnprocessableEntityException(message);
+    } else {
+      return next();
+    }
+  };
+}
