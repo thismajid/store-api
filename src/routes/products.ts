78348d8d7fe93b560c73d31574a5838d1e979905
thismajid <mkhorshidian72@gmail.com>
2022-06-05 17:07:22 +0430

    add validator to product route

--- src/routes/products.ts
+++ src/routes/products.ts
@@ -2,6 +2,8 @@ import { Router } from "express";
 
 import { Routes } from "./../interfaces/route.interface";
 import ProductController from "../controllers/product.controller";
+import { Validator } from "../middlewares/validator.middleware";
+import productSchema from "../validators/product.validator";
 
 export class ProductsRoute implements Routes {
   public router = Router();
@@ -15,7 +17,11 @@ export class ProductsRoute implements Routes {
   private initializeRoutes() {
     this.router.get(this.path, this.controller.getAllProducts);
 
-    this.router.post(this.path, this.controller.addProduct);
+    this.router.post(
+      this.path,
+      Validator(productSchema),
+      this.controller.addProduct
+    );
 
     this.router.get(`${this.path}/:id`, this.controller.getSingleProduct);
 
