20d1d7c4a73d835748de13890a7ffecddb8ab48b
thismajid <mkhorshidian72@gmail.com>
2022-06-07 19:31:12 +0430

    get category by name method

--- src/services/category.service.ts
+++ src/services/category.service.ts
@@ -4,6 +4,18 @@ const prisma = new PrismaClient();
 
 class CategoryService {
   constructor() {}
+
+  public async getCategoryByName(name: string) {
+    try {
+      return await prisma.category.findFirst({
+        where: {
+          name,
+        },
+      });
+    } catch (err) {
+      throw err;
+    }
+  }
 }
 
 export default CategoryService;
