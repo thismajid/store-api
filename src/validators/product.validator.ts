e2d36d3ce538a2aee171427e7c7f49819e85defe
thismajid <mkhorshidian72@gmail.com>
2022-06-09 17:19:32 +0430

    change name to createProductSchema

--- src/validators/product.validator.ts
+++ src/validators/product.validator.ts
@@ -1,13 +1,11 @@
 const Joi = require("joi");
 
-const productSchema = Joi.object({
+const createProductSchema = Joi.object({
   title: Joi.string().min(3).required(),
   description: Joi.string().min(3).required(),
   price: Joi.number().positive().required(),
-  image:
-    Joi.string()
-    .required(),
+  image: Joi.string().required(),
   category: Joi.string().required(),
 });
 
-export default productSchema;
+export { createProductSchema };
