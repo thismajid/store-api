03d627dfe6eafaa220638f5785510890e1185bfd
thismajid <mkhorshidian72@gmail.com>
2022-06-10 17:24:55 +0430

    update server for new route

--- src/server.ts
+++ src/server.ts
@@ -3,8 +3,13 @@ import App from "./app";
 // import IndexRoute from "./routes/index";
 import { ProductsRoute } from "./routes/products";
 import { CategoryRoute } from "./routes/category";
+import { CartRoute } from "./routes/carts";
 
 // @ts-ignore
-const app = new App([new ProductsRoute(), new CategoryRoute()]);
+const app = new App([
+  new ProductsRoute(),
+  new CategoryRoute(),
+  new CartRoute(),
+]);
 
 app.listen();
