26928d8d6405cc62a4a5c3539ebad854f4769049
thismajid <mkhorshidian72@gmail.com>
2022-06-11 19:46:52 +0430

    update users seeders

---
+++ src/prisma/seeds/users.ts
@@ -0,0 +1,32 @@
+export const users = [
+  {
+    email: "admin@admin.com",
+    firstname: "admin",
+    lastname: "admin",
+    password: "123456",
+  },
+  {
+    email: "test1@test.com",
+    firstname: "test",
+    lastname: "test",
+    password: "123456",
+  },
+  {
+    email: "test2@test.com",
+    firstname: "test",
+    lastname: "test",
+    password: "123456",
+  },
+  {
+    email: "test3@test.com",
+    firstname: "test",
+    lastname: "test",
+    password: "123456",
+  },
+  {
+    email: "test4@test.com",
+    firstname: "test",
+    lastname: "test",
+    password: "123456",
+  },
+];
