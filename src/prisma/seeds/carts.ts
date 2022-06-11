7a83acf6a9e28778e62d2853ca6b86a33020e61c
thismajid <mkhorshidian72@gmail.com>
2022-06-11 19:46:22 +0430

    add carts seeder

---
+++ src/prisma/seeds/carts.ts
@@ -0,0 +1,47 @@
+export const carts = [
+  {
+    userId: 1,
+    cartItems: {
+      create: [
+        {
+          productId: 2,
+          quantity: 1,
+        },
+        {
+          productId: 3,
+          quantity: 2,
+        },
+      ],
+    },
+  },
+  {
+    userId: 2,
+    cartItems: {
+      create: [
+        {
+          productId: 18,
+          quantity: 2,
+        },
+        {
+          productId: 13,
+          quantity: 3,
+        },
+      ],
+    },
+  },
+  {
+    userId: 3,
+    cartItems: {
+      create: [
+        {
+          productId: 7,
+          quantity: 1,
+        },
+      ],
+    },
+  },
+  {
+    userId: 4,
+    cartItems: {},
+  },
+];
