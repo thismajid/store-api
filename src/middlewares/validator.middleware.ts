f65095d36493ced8d04a1ceaab693ea1c444b511
thismajid <mkhorshidian72@gmail.com>
2022-06-17 11:57:38 +0430

    changes in input of validator middleware

--- src/middlewares/validator.middleware.ts
+++ src/middlewares/validator.middleware.ts
@@ -5,7 +5,8 @@ import UnprocessableEntityException from "../exceptions/UnprocessableEntityExcep
 
 export function Validator(schema: ObjectSchema) {
   return (req: Request, res: Response, next: NextFunction) => {
-    const params = req.method === "GET" ? req.params : req.body;
+    const params =
+      req.method === "GET" ? req.params : { ...req.params, ...req.body };
     const { error } = schema.validate(params, {
       abortEarly: false,
       messages: {
