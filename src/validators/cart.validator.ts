fc086b2f4f76943802ae50606cee5ff0e274c61a
thismajid <mkhorshidian72@gmail.com>
2022-06-17 12:01:07 +0430

    add update cart items schema

--- src/validators/cart.validator.ts
+++ src/validators/cart.validator.ts
@@ -4,4 +4,14 @@ const getSingleCartSchema = Joi.object({
   id: Joi.number().integer().required(),
 });
 
-export { getSingleCartSchema };
+const updateCartItemsSchema = Joi.object({
+  id: Joi.number().integer().required(),
+  products: Joi.array().items(
+    Joi.object({
+      productId: Joi.number().integer().required(),
+      quantity: Joi.number().integer().required(),
+    })
+  ),
+});
+
+export { getSingleCartSchema, updateCartItemsSchema };
