a9dbddb25e14e223ac0ee6c60025f1866f42b261
thismajid <mkhorshidian72@gmail.com>
2022-06-20 20:04:44 +0430

    add get single category controller

--- src/controllers/category.controller.ts
+++ src/controllers/category.controller.ts
@@ -3,8 +3,10 @@ import { PrismaClient } from "@prisma/client";
 
 import logger from "../utils/logger";
 import Redisio from "../services/redis.service";
+import CategoryService from "../services/category.service";
 
 const prisma = new PrismaClient();
+const categoriesService = new CategoryService();
 const categoriesRedis = new Redisio("categories");
 
 class CategoryController {
@@ -25,6 +27,27 @@ class CategoryController {
       categories,
     });
     try {
+    } catch (err) {
+      logger.error(err);
+    }
+  }
+
+  public async getSingleCategory(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) {
+    try {
+      const { id } = req.params;
+      let category = await categoriesRedis.hget("categories", { id: +id });
+      if (!category) {
+        category = await categoriesService.getSingleCategory(+id);
+        categoriesRedis.hmset(category, "categories");
+      }
+      res.json({
+        message: `category with id: ${id} retrieved successfully`,
+        category,
+      });
     } catch (err) {
       logger.error(err);
     }
